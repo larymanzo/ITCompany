a53e0bd0909478c66581a7604596bd77dea87304
Larissa Manzo Thosi <lamosinha@hotmail.com>
2026-02-09 15:48:38 -0300

    Fix tailwind working

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Bem-vindo ao Next.js</h1>
+    <main className=" bg-green-950">
+      <h1 className="text-3xl font-bold">Bem-vindo ao Next.js seu arrombado</h1>
       <p>Aplicação criada pelo assistente.</p>
       <p><Link href="/about">Sobre</Link></p>
     </main>
